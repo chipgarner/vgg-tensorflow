bbdf9906abfe86f0878822113808371f8fbb0acc
jkg <chip@garnertotalenergy.com>
2017-10-19 20:21:51 -0700

    Refactoring

--- Pbfile.py
+++ Pbfile.py
@@ -4,6 +4,10 @@ from scipy.misc import imread, imresize
 
 from pre_trained.imagenet_classes import class_names
 import Paths
+import vgg16
+
+from tensorflow.python.framework.graph_util_impl import convert_variables_to_constants
+from tensorflow.python.platform import gfile
 
 
 def load_graph(frozen_graph_filename):
@@ -18,8 +22,32 @@ def load_graph(frozen_graph_filename):
     return graph
 
 
+def save_graph_to_file(sess, graph, graph_file_name):
+    output_graph_def = convert_variables_to_constants(
+        sess, graph.as_graph_def(), ['final_tensor'])
+    with gfile.FastGFile(graph_file_name, 'wb') as f:
+        f.write(output_graph_def.SerializeToString())
+    return
+
+
+def save_vgg_to_pb_file():
+    directory = Paths.this_directory()
+
+    sess = tf.Session()
+    imgs = tf.placeholder(tf.float32, [None, 224, 224, 3])
+    vgg16.vgg16(imgs, directory + '/pre_trained/vgg16_weights.npz', sess)
+
+    save_graph_to_file(sess, sess.graph, 'out/output_graph.pb')
+    # This is for humans
+    with gfile.FastGFile('out/output_labels.txt', 'w') as f:
+        f.write('\n'.join(class_names) + '\n')
+
+
 if __name__ == '__main__':
-    # load a .pb file and try and image
+
+    save_vgg_to_pb_file()
+
+    # load a .pb file and try an image
     directory = Paths.this_directory()
     graph = load_graph(directory + '/out/output_graph.pb')
 
